5fdd3555eb271dc03dd2de3821b98374f72197f6
berkegeoguz <berkegemenoguz@hotmail.com>
2026-08-07 21:03:20 +0300

    Add TR/EN language toggle with cookie-based persistence

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, g
 from bist import bist_sirketler, bist_detay_veri
 from viop import viop_ozet_veri, viop_detay_veri, kontrat_tarih_araligi
 from graphicgenerator import grafik_ciz_html
+from translations import TRANSLATIONS
 
 app = Flask(__name__)
+
+
+@app.before_request
+def set_language():
+    g.lang = request.cookies.get("lang", "en")
+    g.t = TRANSLATIONS.get(g.lang, TRANSLATIONS["en"])
+
+
+@app.context_processor
+def inject_translations():
+    return {"t": g.t, "lang": g.lang}
+
+
+@app.route("/lang/<code>")
+def set_lang(code):
+    if code not in TRANSLATIONS:
+        code = "en"
+    resp = make_response("")
+    resp.set_cookie("lang", code, max_age=365*24*3600)
+    resp.headers["Location"] = request.referrer or "/"
+    resp.status_code = 302
+    return resp
 
 
 @app.route("/")
